30896998b0c40ee79be05eb4665ac4ae80584cbe
prandeveloper <pranaydevelopersveltosest@gmail.com>
2023-08-05 17:04:56 +0530

    build made

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4fc3b8b1874365ba089eca3a6e388229.js"
+  "/precache-manifest.28b8e7d364ede4d675ee4538140ee35f.js"
 );
 
 self.addEventListener('message', (event) => {
--- build/static/js/177.6a16b6ac.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[177],{2017:function(a,s){}}]);
-//# sourceMappingURL=177.6a16b6ac.chunk.js.map
